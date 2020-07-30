07e8dee0d8bdfd5d11525582098e8bdf95a22033
Pavel Vladimirovich <sawel@yandex.ru>
2020-07-30 16:02:01 +0800

    init

---
+++ src/AlgorithmsDataStructures2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class AlgorithmsDataStructures2 {
+    public static int[] GenerateBBSTArray(int[] a) {
+        int[] resAr = new int[a.length];
+        Arrays.sort(a);
+        recurs(a, resAr, 0);
+        return resAr;
+    }
+
+    public static int[] recurs(int[] arr, int[] resArr, int indexToAdd) {
+        if (arr.length == 1) {  // condition for end recursion
+            resArr[indexToAdd] = arr[0];
+            return resArr;
+        }
+
+        int length = arr.length;
+        int indexCenter = length / 2;
+
+        resArr[indexToAdd] = arr[indexCenter];
+        int[] arrLeft = Arrays.copyOfRange(arr, 0, indexCenter);
+        recurs(arrLeft, resArr, indexToAdd * 2 + 1);
+        int[] arrRight = Arrays.copyOfRange(arr, indexCenter + 1, length);
+        recurs(arrRight, resArr, indexToAdd * 2 + 2);
+
+        return resArr;
+    }
+}
